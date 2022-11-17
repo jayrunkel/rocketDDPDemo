f6c84b14e1589f9f7fd931beeb287f41d34e7910
Jay Runkel <jayrunkel@gmail.com>
2022-11-17 18:08:00 -0500

    added trigger code

---
+++ triggers/OPPS_Range_M1_Trigger.js
@@ -0,0 +1,63 @@
+exports = function(changeEvent) {
+  /*
+    A Database Trigger will always call a function with a changeEvent.
+    Documentation on ChangeEvents: https://docs.mongodb.com/manual/reference/change-events/
+
+    Access the _id of the changed document:
+    const docId = changeEvent.documentKey._id;
+
+    Access the latest version of the changed document
+    (with Full Document enabled for Insert, Update, and Replace operations):
+    const fullDocument = changeEvent.fullDocument;
+
+    const updateDescription = changeEvent.updateDescription;
+
+    See which fields were changed (if any):
+    if (updateDescription) {
+      const updatedFields = updateDescription.updatedFields; // A document containing updated fields
+    }
+
+    See which fields were removed (if any):
+    if (updateDescription) {
+      const removedFields = updateDescription.removedFields; // An array of removed fields
+    }
+
+    Functions run by Triggers are run as System users and have full access to Services, Functions, and MongoDB Data.
+
+    Access a mongodb service:
+    const collection = context.services.get(<SERVICE_NAME>).db("db_name").collection("coll_name");
+    const doc = collection.findOne({ name: "mongodb" });
+
+    Note: In Atlas Triggers, the service name is defaulted to the cluster name.
+
+    Call other named functions if they are defined in your application:
+    const result = context.functions.execute("function_name", arg1, arg2);
+
+    Access the default http client and execute a GET request:
+    const response = context.http.get({ url: <URL> })
+
+    Learn more about http client here: https://docs.mongodb.com/realm/functions/context/#context-http
+  */
+  
+  let noteDoc = {
+		title: 'OUT OF BOUNDS NOTIFICATION',
+		notes: `Parameter ${parsedField.fieldName} was out of bounds. 
+${parsedField.fieldType} was exceeded.
+
+Parameter: ${parsedField.fieldName},
+Value: ${doc[parsedField.fieldName]},
+Timestamp: ${doc.time}
+`,
+		type: "outOfBoundsNotification",
+		device: doc["meta"]["device"],
+		parameter: parsedField.fieldName,
+		value: doc[parsedField.fieldName],
+		violationType: parsedField.fieldType,
+		timeStamp: doc.time,
+		author : {
+			name: "Atlas Triggers"
+		}
+	};
+
+	db.notes.insertOne(noteDoc);
+};
